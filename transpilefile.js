473f8251a997c794c325a76b8c109fce020c2b5f
pihart <avi.mehra1@gmail.com>
2020-09-19 23:21:12 -0700

    Add WHILE support to file suffix renaming for node version

--- transpilefile.js
+++ transpilefile.js
@@ -3,7 +3,7 @@ const transpile = require("./transpiler");
 
 const transpileFile = (fileName, destinationFileName) => {
     const program = fs.readFileSync(fileName).toString();
-    fs.writeFile(destinationFileName || fileName.replace(".loop", ".transpiled.loop"),
+    fs.writeFile(destinationFileName || fileName.replace(".loop", ".transpiled.loop").replace(".while", ".transpiled.while"),
         transpile(program).join("\n"), console.log);
 }
 
